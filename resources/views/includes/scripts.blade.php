b4af949c1b3a1c6ca46be5724be07a51d0324b6c
vinicius_mooven <vinicius.silva@moovenconsulting.com.br>
2018-05-09 05:58:09 -0300

    update

---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,3 @@
+<script src="{{asset('libs/jquery/jquery-3.2.1.slim.min.js')}}" crossorigin="anonymous"></script>
+<script src="{{asset('libs/popper/popper.min.js')}}" crossorigin="anonymous"></script>
+<script src="{{asset('libs/bootstrap/js/bootstrap.min.js')}}" crossorigin="anonymous"></script>
